c123160ab9c9af9658119b3cd007a4e8a53e002f
Rocean <roceanadhikari@gmail.com>
2026-01-26 19:06:26 +0530

    removed the subdirectories

--- backend/gold_silver_tracker/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('prices/', views.get_prices, name='get_prices'),
-    path('health/', views.health_check, name='health_check'),
-]
