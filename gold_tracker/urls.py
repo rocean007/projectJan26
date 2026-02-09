ea7bdb410844a79fa1720a45a49fdb5608bc0f18
Rocean <roceanadhikari@gmail.com>
2026-02-09 17:21:29 +0530

    renamed:    templates/favicon.webp -> favicon.webp

--- gold_tracker/urls.py
+++ gold_tracker/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('api/', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('favicon.webp', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.webp', permanent=True)),
 ]
